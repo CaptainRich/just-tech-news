2ac2873eb87c0d8106ee84fd1e26a02f6c445aeb
RichA <rich_ay@sbcglobal.net>
2020-10-21 22:12:07 -0500

    Clean up JAWSDB implementation.

--- config/connection.js
+++ config/connection.js
@@ -8,16 +8,15 @@ require('dotenv').config();
 // Create the connection to our database, pass in your MySQL information for username and password
 //const sequelize = new Sequelize('just_tech_news_db', 'root', 'Learning_2020', {
 
-Let sequelize;
+  let sequelize;
 
-if(process.env.JAWSDB_RUL) {
-  sequelize = new Sequelize( process.env.JAWSDB_URL );
-} else {
-  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
-  
+  if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+  }
   module.exports = sequelize;
