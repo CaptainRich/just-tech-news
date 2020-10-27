148247a5568bea1d96def3be1d1d056610623cdc
RichA <rich_ay@sbcglobal.net>
2020-10-27 15:40:28 -0500

    Clean up / fixes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -118,12 +118,12 @@ router.put('/upvote', (req, res) => {
   // Use the custom static method created in models/Post.js
   // Make sure the session exists first.
   if (req.session) {
-    // Pass the session ID along with all destructured properties on req.body
-    Post.upvote(req.body, { Vote })
-      .then(updatedPostData => res.json(updatedPostData))
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.status(500).json(err);
       });
   }
 });
