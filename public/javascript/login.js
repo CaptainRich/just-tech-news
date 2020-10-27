2b9d3aa1b85542bb51b1bb3d7c2441abe90da6d2
RichA <rich_ay@sbcglobal.net>
2020-10-26 22:21:57 -0500

    Fixed login function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -26,7 +26,7 @@ async function signupFormHandler(event) {
 
         // Check the response status
         if( response.ok ) {
-            console.log( 'Success' );
+            console.log( 'Successful signup!' );
         }
         else {
             alert( response.statusText );
@@ -42,8 +42,8 @@ async function loginFormHandler(event) {
     event.preventDefault();
 
     // Get the data from the form
-    const email    = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const email    = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
     if (email && password) {  // Only do this if all data exists
         const response = await fetch('/api/users/login', {
