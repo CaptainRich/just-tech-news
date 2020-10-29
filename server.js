77a921c2029cec8d304638a76dafd673b95dcd5f
RichA <rich_ay@sbcglobal.net>
2020-10-28 23:30:59 -0500

    Pluralization works

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const express        = require('express');
 const routes         = require('./controllers');
 const sequelize      = require('./config/connection');
+const helpers        = require('./utils/helpers');
 
-const hbs = exphbs.create({});
+  const hbs = exphbs.create( {helpers} );
 
 const sess = {
   secret: 'Super secret secret',   // replace this with a real password in the '.env' file
