a5cb898424b57c826434939edae962bac7f7d0e0
RichA <rich_ay@sbcglobal.net>
2022-05-24 21:02:57 -0500

    Improving code formatting and comments.

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,20 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Create the connection to our database, pass in your MySQL information for username and password
-//const sequelize = new Sequelize('just_tech_news_db', 'root', 'Learning_2020', {
+//const sequelize = new Sequelize('just_tech_news_db', 'root', 'xxxxxxxxxxx', {
 
   let sequelize;
 
-  if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+} else {
+  // The variables DB_NAME, DB_USER, and DB_PW are defined in the ".env" file, accessed via the DOTENV package.
+   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
-    });
-  }
+  });
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
   module.exports = sequelize;
